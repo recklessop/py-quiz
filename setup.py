44dda6c969cbb282790e09a754b0d4c424696ea4
Justin Paul <justin@jpaul.me>
2023-09-06 13:42:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='quiz',
+    name='pyquizjp',
     version='0.1',
     description='A quiz module for interactive Python quizzes',
     author='Justin Paul',
     author_email='justin@jpaul.me',
-    packages=['quiz'],
+    packages=['pyquizjp'],
     install_requires=[
         # List any dependencies your module may have here
         'ipywidgets',
