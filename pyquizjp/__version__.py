bd0a0eeea70a8f569a4b97bd3132588b93c9f3db
Justin Paul <justin@jpaul.me>
2023-09-06 18:37:24 -0400

    Create __version__.py

---
+++ pyquizjp/__version__.py
@@ -0,0 +1,9 @@
+__title__ = "pyquizjp"
+__description__ = "Python quiz module from recklessop"
+__url__ = "https://github.com/recklessop/pyquizjp"
+__version__ = "0.5.0"
+__build__ = 0x023100
+__author__ = "Justin Paul"
+__author_email__ = "justin@jpaul.me"
+__license__ = "GPL-3.0"
+__copyright__ = "Copyright Justin Paul"
