f21dac2536c54f6b5b115e970f497edfa14212d8
Justin Paul <justin@jpaul.me>
2023-09-06 18:19:30 -0400

    Update quiz.py

--- pyquizjp/quiz.py
+++ pyquizjp/quiz.py
@@ -35,23 +35,37 @@ class Quiz:
             clear_output(wait=True)
             self.display_result()
 
-    def display_result(self):
+def display_result(self):
         correct_answers = sum(response == question['answer'] for response, question in zip(self.user_responses, self.questions))
         total_questions = len(self.questions)
-        result_text = f'You got {correct_answers} out of {total_questions} questions correct!'
-        
-        # Display the result
-        display(HTML(f'<div style="font-size: larger; text-align: center;">{result_text}</div>'))
-        
-        # Display the questions with highlighting
+        result_text = f'You got {correct_answers} out of {total_questions} questions correct!<br><br>'
+
+        # Create a list to store the incorrect questions and their details
+        incorrect_questions = []
+
         for i, (user_response, question) in enumerate(zip(self.user_responses, self.questions)):
-            question_text = question['question']
-            correct_answer = question['answer']
-            if user_response != correct_answer:
-                # Highlight incorrect user response in red and correct answer in green
-                question_text = question_text.replace(user_response, f'<span style="color: red;">{user_response}</span>')
-                question_text = question_text.replace(correct_answer, f'<span style="color: green;">{correct_answer}</span>')
-            display(HTML(f'<strong>Question {i + 1}:</strong> {question_text}'))
+            if user_response != question['answer']:
+                incorrect_questions.append({
+                    'question_text': question['question'],
+                    'correct_answer': question['answer'],
+                    'user_response': user_response
+                })
+
+        if len(incorrect_questions) > 0:
+            result_text += '<strong>Incorrect Questions:</strong><br>'
+            for i, incorrect_question in enumerate(incorrect_questions, 1):
+                question_text = incorrect_question['question_text']
+                correct_answer = incorrect_question['correct_answer']
+                user_response = incorrect_question['user_response']
+                result_text += f'{i}. {question_text}<br>'
+                result_text += f'   Correct Answer: <span style="color: green;">{correct_answer}</span><br>'
+                result_text += f'   Your Answer: <span style="color: red;">{user_response}</span><br>'
+
+        self.feedback_text.value = result_text
+
+        # Clear the previous widget display and display the result
+        self.widget_container.children = []
+        self.widget_container.children = [self.feedback_text]
 
     def load_questions_from_url(self, url):
         try:
