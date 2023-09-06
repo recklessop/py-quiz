10fec76926cae347419350af6ccabd66f790722f
Justin Paul <justin@jpaul.me>
2023-09-06 18:37:55 -0400

    Update __init__.py

--- pyquizjp/__init__.py
+++ pyquizjp/__init__.py
@@ -1,1 +1,12 @@
 from .quiz import Quiz
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __build__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
