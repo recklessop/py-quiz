ed5c0001e44c92dc3f5f14f73aaa3129fabe0f22
Justin Paul <justin@jpaul.me>
2023-09-06 19:03:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyquizjp',
-    version='0.5.3',
+    version='0.5.4',
     description='A quiz module for interactive Python quizzes',
     author='Justin Paul',
     author_email='justin@jpaul.me',
