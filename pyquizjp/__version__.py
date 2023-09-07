5797cf0557fc632f5ff6fb4bdfa6acee1963bb4d
Justin Paul <justin@jpaul.me>
2023-09-06 20:35:34 -0400

    Update __version__.py

--- pyquizjp/__version__.py
+++ pyquizjp/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "pyquizjp"
 __description__ = "Python quiz module from recklessop"
 __url__ = "https://github.com/recklessop/pyquizjp"
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 __build__ = 0x023100
 __author__ = "Justin Paul"
 __author_email__ = "justin@jpaul.me"
